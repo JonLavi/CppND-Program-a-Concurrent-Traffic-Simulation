0ad38149791090f0cc7c770011615382de61a4f5
Jonathan Lavi <jonathan.lavi@gmail.com>
2020-07-16 14:32:24 +0100

    gitignore, rm build

--- build/CMakeFiles/traffic_simulation.dir/DependInfo.cmake
+++ build/CMakeFiles/traffic_simulation.dir/DependInfo.cmake
@@ -4,19 +4,19 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/mnt/c/Users/jon10278.AVWORLD/git/udacity_cpp/CppND-Program-a-Concurrent-Traffic-Simulation/src/Graphics.cpp" "/mnt/c/Users/jon10278.AVWORLD/git/udacity_cpp/CppND-Program-a-Concurrent-Traffic-Simulation/build/CMakeFiles/traffic_simulation.dir/src/Graphics.cpp.o"
-  "/mnt/c/Users/jon10278.AVWORLD/git/udacity_cpp/CppND-Program-a-Concurrent-Traffic-Simulation/src/Intersection.cpp" "/mnt/c/Users/jon10278.AVWORLD/git/udacity_cpp/CppND-Program-a-Concurrent-Traffic-Simulation/build/CMakeFiles/traffic_simulation.dir/src/Intersection.cpp.o"
-  "/mnt/c/Users/jon10278.AVWORLD/git/udacity_cpp/CppND-Program-a-Concurrent-Traffic-Simulation/src/Street.cpp" "/mnt/c/Users/jon10278.AVWORLD/git/udacity_cpp/CppND-Program-a-Concurrent-Traffic-Simulation/build/CMakeFiles/traffic_simulation.dir/src/Street.cpp.o"
-  "/mnt/c/Users/jon10278.AVWORLD/git/udacity_cpp/CppND-Program-a-Concurrent-Traffic-Simulation/src/TrafficLight.cpp" "/mnt/c/Users/jon10278.AVWORLD/git/udacity_cpp/CppND-Program-a-Concurrent-Traffic-Simulation/build/CMakeFiles/traffic_simulation.dir/src/TrafficLight.cpp.o"
-  "/mnt/c/Users/jon10278.AVWORLD/git/udacity_cpp/CppND-Program-a-Concurrent-Traffic-Simulation/src/TrafficObject.cpp" "/mnt/c/Users/jon10278.AVWORLD/git/udacity_cpp/CppND-Program-a-Concurrent-Traffic-Simulation/build/CMakeFiles/traffic_simulation.dir/src/TrafficObject.cpp.o"
-  "/mnt/c/Users/jon10278.AVWORLD/git/udacity_cpp/CppND-Program-a-Concurrent-Traffic-Simulation/src/TrafficSimulator-Final.cpp" "/mnt/c/Users/jon10278.AVWORLD/git/udacity_cpp/CppND-Program-a-Concurrent-Traffic-Simulation/build/CMakeFiles/traffic_simulation.dir/src/TrafficSimulator-Final.cpp.o"
-  "/mnt/c/Users/jon10278.AVWORLD/git/udacity_cpp/CppND-Program-a-Concurrent-Traffic-Simulation/src/Vehicle.cpp" "/mnt/c/Users/jon10278.AVWORLD/git/udacity_cpp/CppND-Program-a-Concurrent-Traffic-Simulation/build/CMakeFiles/traffic_simulation.dir/src/Vehicle.cpp.o"
+  "/Volumes/Data/Users/jonathan/code/udacity_cpp/CppND-Program-a-Concurrent-Traffic-Simulation/src/Graphics.cpp" "/Volumes/Data/Users/jonathan/code/udacity_cpp/CppND-Program-a-Concurrent-Traffic-Simulation/build/CMakeFiles/traffic_simulation.dir/src/Graphics.cpp.o"
+  "/Volumes/Data/Users/jonathan/code/udacity_cpp/CppND-Program-a-Concurrent-Traffic-Simulation/src/Intersection.cpp" "/Volumes/Data/Users/jonathan/code/udacity_cpp/CppND-Program-a-Concurrent-Traffic-Simulation/build/CMakeFiles/traffic_simulation.dir/src/Intersection.cpp.o"
+  "/Volumes/Data/Users/jonathan/code/udacity_cpp/CppND-Program-a-Concurrent-Traffic-Simulation/src/Street.cpp" "/Volumes/Data/Users/jonathan/code/udacity_cpp/CppND-Program-a-Concurrent-Traffic-Simulation/build/CMakeFiles/traffic_simulation.dir/src/Street.cpp.o"
+  "/Volumes/Data/Users/jonathan/code/udacity_cpp/CppND-Program-a-Concurrent-Traffic-Simulation/src/TrafficLight.cpp" "/Volumes/Data/Users/jonathan/code/udacity_cpp/CppND-Program-a-Concurrent-Traffic-Simulation/build/CMakeFiles/traffic_simulation.dir/src/TrafficLight.cpp.o"
+  "/Volumes/Data/Users/jonathan/code/udacity_cpp/CppND-Program-a-Concurrent-Traffic-Simulation/src/TrafficObject.cpp" "/Volumes/Data/Users/jonathan/code/udacity_cpp/CppND-Program-a-Concurrent-Traffic-Simulation/build/CMakeFiles/traffic_simulation.dir/src/TrafficObject.cpp.o"
+  "/Volumes/Data/Users/jonathan/code/udacity_cpp/CppND-Program-a-Concurrent-Traffic-Simulation/src/TrafficSimulator-Final.cpp" "/Volumes/Data/Users/jonathan/code/udacity_cpp/CppND-Program-a-Concurrent-Traffic-Simulation/build/CMakeFiles/traffic_simulation.dir/src/TrafficSimulator-Final.cpp.o"
+  "/Volumes/Data/Users/jonathan/code/udacity_cpp/CppND-Program-a-Concurrent-Traffic-Simulation/src/Vehicle.cpp" "/Volumes/Data/Users/jonathan/code/udacity_cpp/CppND-Program-a-Concurrent-Traffic-Simulation/build/CMakeFiles/traffic_simulation.dir/src/Vehicle.cpp.o"
   )
-set(CMAKE_CXX_COMPILER_ID "GNU")
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
 
 # The include file search paths:
 set(CMAKE_CXX_TARGET_INCLUDE_PATH
-  "/mnt/c/tools/opencv/build/include"
+  "/usr/local/Cellar/opencv/4.3.0_5/include/opencv4"
   )
 
 # Targets to which this target links.
